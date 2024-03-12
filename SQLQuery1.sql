8d7bc06a5d560745032dcd92f19ce195315c740d
HoangNguyen <157053838+cunbeo1155@users.noreply.github.com>
2024-03-12 12:42:29 +0700

    update
    update_filesql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,4 @@
-create table THAMGIADT
+Ôªøcreate table THAMGIADT
 (MAGV nchar(3) not null,
 MADT nchar(4) not null,
 STT int not null,
@@ -70,99 +70,99 @@ NGAYKT datetime,
 CONSTRAINT PK_CONGVIEC PRIMARY KEY (MADT, SOTT)
 )
 
---Nh?p data cho b?ng CHUDE
+--Nhap data cho bang CHUDE
 insert into CHUDE(MACD, TENCD)
 values
-('NCPT', N'NghiÍn c?u ph·t tri?n'),
-('QLGD', N'Qu?n l˝ gi·o d?c'),
-('UDCN', N'?ng d?ng cÙng ngh?')
+('NCPT', N'Nghien cuu phat trien'),
+('QLGD', N'Quan ly giao duc'),
+('UDCN', N'Ung dung cong nghe')
 
---Nh?p data cho b?ng BOMON
+--Nhap data cho bang BOMON
 insert into BOMON(MABM, TENBM, PHONG, DIENTHOAI, MAKHOA)
 values
-(N'CNTT',N'CÙng ngh? tri th?c','B15','0838126126',N'CNTT'),
-(N'HHC',N'HÛa h?u c?','B44','0838222222',N'HH'),
-(N'HL',N'HÛa L˝','B42','0838878787',N'HH')
+(N'CNTT',N'Cong nghe tri thuc','B15','0838126126',N'CNTT'),
+(N'HHC',N'Hoa huu co','B44','0838222222',N'HH'),
+(N'HL',N'Hoa ly','B42','0838878787',N'HH')
 
 insert into BOMON(MABM,TENBM,PHONG,DIENTHOAI,TRUONGBM,MAKHOA,NGAYNHANCHUC)
 values
-(N'HPT',N'HÛa ph‚n tÌch','B43','0838777777','007',N'HH','2007-10-15'),
-(N'HTTT',N'H? th?ng thÙng tin','B13','0838125125','002',N'CNTT','2004-09-20'),
-(N'MMT',N'M?ng m·y tÌnh','B16','0838676767','001',N'CNTT','2005-05-15')
+(N'HPT',N'Hoa phan tich','B43','0838777777','007',N'HH','2007-10-15'),
+(N'HTTT',N'He thong thong tin','B13','0838125125','002',N'CNTT','2004-09-20'),
+(N'MMT',N'Mang may tinh','B16','0838676767','001',N'CNTT','2005-05-15')
 
 Insert Into BOMON(MABM,TENBM,PHONG,DIENTHOAI,MAKHOA)
 values
-(N'SH',N'Sinh hÛa','B33','0838898989',N'SH'),
-(N'VL?T',N'V?t l˝ ?i?n t?','B23','0838234234',N'VL')
+(N'SH',N'Sinh hoa','B33','0838898989',N'SH'),
+(N'VLDT',N'Vat ly dien tu','B23','0838234234',N'VL')
 
 
 insert into BOMON(MABM,TENBM,PHONG,DIENTHOAI,TRUONGBM,MAKHOA,NGAYNHANCHUC)
 values
-(N'VLUD',N'V?t l˝ ?ng d?ng','B24','0838454545','005',N'VL','2006-02-18'),
+(N'VLUD',N'Vat ly ung dung','B24','0838454545','005',N'VL','2006-02-18'),
 (N'VS',N'Vi Sinh','B32','0838909090','004',N'SH','2007-01-01')
 
 
---Nh?p data cho b?ng GIAOVIEN
+--Nhap data cho bang GIAOVIEN
 Insert Into GIAOVIEN(MAGV,HOTEN,LUONG,PHAI,NGSINH,DIACHI,MABM)
 values
-('001',N'Nguy?n Ho‡i An',2000.0,N'Nam','1973-02-15',N'25/3 L?c Long Qu‚n, Q.10,TP HCM', N'HHC'),
-('002',N'Tr?n Tr‡ H??ng',2500.0,N'N?','1960-06-20',N'125 Tr?n H?ng ??o, Q.1, TP HCM', N'VLUD') 
+('001',N'Nguyen Hoai An',2000.0,N'Nam','1973-02-15',N'25/3 Lac Long Quan, Q.10,TP HCM', N'HHC'),
+('002',N'Tran Tra Huong',2500.0,N'Nu','1960-06-20',N'125 Tran Hung Dao, Q.1, TP HCM', N'VLUD') 
 
 Insert Into GIAOVIEN(MAGV,HOTEN,LUONG,PHAI,NGSINH,DIACHI,GVQLCM, MABM)
 values
-('003',N'Nguy?n Ng?c ¡nh',2200.0,N'N?','1975-05-11',N'12/21 Vı V?n Ng‚n Th? ??c, TP HCM',N'002',N'VL?T') 
+('003',N'Nguyen Ngoc Anh',2200.0,N'Nu','1975-05-11',N'12/21 Vo Van Ngan Thu Duc, TP HCM',N'002',N'VLDT') 
 
 Insert Into GIAOVIEN(MAGV,HOTEN,LUONG,PHAI,NGSINH,DIACHI,MABM)
 values
-('004',N'Tr??ng Nam S?n',2300.0,N'Nam','1959-06-20',N'215 L˝ Th??ng Ki?t,TP BiÍn HÚa',N'HPT'),
-('005',N'L˝ Ho‡ng H‡',2500.0,N'Nam','1954-10-23',N'22/5 Nguy?n XÌ, Q.BÏnh Th?nh, TP HCM',N'SH')
+('004',N'Truong Nam Son',2300.0,N'Nam','1959-06-20',N'215 Ly Thuong Kiet, TP Bien Hoa',N'HPT'),
+('005',N'Ly Hoang Ha',2500.0,N'Nam','1954-10-23',N'22/5 Nguyen Xi, Q.Binh Thanh, TP HCM',N'SH')
 
 Insert Into GIAOVIEN(MAGV,HOTEN,LUONG,PHAI,NGSINH,DIACHI,GVQLCM, MABM)
 values
-('006',N'Tr?n B?ch Tuy?t',1500.0,N'N?','1980-05-20',N'127 H˘ng V??ng, TP M? Tho',N'004', N'MMT')
+('006',N'Tran Bach Tuyet',1500.0,N'Nu','1980-05-20',N'127 Hung Vuong, TP My Tho',N'004', N'MMT')
 
 
 Insert Into GIAOVIEN(MAGV,HOTEN,LUONG,PHAI,NGSINH,DIACHI, MABM)
 values
-('007',N'Nguy?n An Trung',2100.0,N'Nam','1976-06-05',N'234 3/2, TP BiÍn HÚa', N'HL')
+('007',N'Nguyen An Trung',2100.0,N'Nam','1976-06-05',N'234 3/2, TP Bien Hoa', N'HL')
 
 Insert Into GIAOVIEN(MAGV,HOTEN,LUONG,PHAI,NGSINH,DIACHI,GVQLCM, MABM) 
 values
-('008',N'Tr?n Trung Hi?u',1800.0,N'Nam','1977-08-06',N'22/11 L˝ Th??ng Ki?t,TP M? Tho',N'007', N'VS'),
-('009',N'Tr?n Ho‡ng nam',2000.0,N'Nam','1975-11-22',N'234 Tr?n N„o,An Ph˙, TP HCM',N'001', N'CNTT'),
-('010',N'Ph?m Nam Thanh',1500.0,N'Nam','1980-12-12',N'221 H˘ng V??ng,Q.5, TP HCM',N'007',N'HTTT') 
+('008',N'Tran Trung Hieu',1800.0,N'Nam','1977-08-06',N'22/11 Ly Thuong Kiet,TP My Tho',N'007', N'VS'),
+('009',N'Tran Hoang nam',2000.0,N'Nam','1975-11-22',N'234 Tran Nao,An Phu, TP HCM',N'001', N'CNTT'),
+('010',N'Pham Nam Thanh',1500.0,N'Nam','1980-12-12',N'221 Hung Vuong,Q.5, TP HCM',N'007',N'HTTT') 
 
 
 
 
---Nh?p data cho b?ng DETAI
+--Nh·∫≠p data cho b·∫£ng DETAI
 Insert Into DETAI(MADT,TENDT,CAPQL,KINHPHI,NGAYBD,NGAYKT,MACD,GVCNDT)
 values
-('001',N'HTTT qu?n l˝ c·c tr??ng ?H',N'?HQG',20.0,'2007-10-20','2008-10-20',N'QLGD','002'),
-('002',N'HTTT qu?n l˝ gi·o v? cho m?t Khoa',N'Tr??ng',20.0,'2000-10-12','2001-10-12',N'QLGD','002'),
-('003',N'NghiÍn c?u ch? t?o s?i NanÙ Platin',N'?HQG',300.0,'2008-05-15','2010-05-15',N'NCPT','005'),
-('004',N'T?o v?t li?u sinh h?c b?ng m‡ng ?i ng??i',N'Nh‡ n??c',100.0,'2007-01-01','2009-12-31',N'NCPT','004'),
-('005',N'?ng d?ng hÛa h?c xanh',N'Tr??ng',200.0,'2003-10-10','2004-12-10',N'UDCN','007'),
-('006',N'NghiÍn c?u t? b‡o g?c',N'Nh‡ n??c',4000.0,'2006-10-12','2009-10-12',N'NCPT','004'),
-('007',N'HTTT qu?n l˝ th? vi?n ? c·c tr??ng ?H',N'Tr??ng',20.0,'2009-05-10','2010-05-10',N'QLGD','001')
+('001',N'HTTT quan ly cac truong ƒêH',N'ƒêHQG',20.0,'2007-10-20','2008-10-20',N'QLGD','002'),
+('002',N'HTTT quan ly giao vu cho mot Khoa',N'Truong',20.0,'2000-10-12','2001-10-12',N'QLGD','002'),
+('003',N'Nghien cuu che tao soi Nano Platin',N'ƒêHQG',300.0,'2008-05-15','2010-05-15',N'NCPT','005'),
+('004',N'Tao vat lieu sinh hoc bang mang oi nguoi',N'Nha nuoc',100.0,'2007-01-01','2009-12-31',N'NCPT','004'),
+('005',N'Ung dung hoa hoc xanh',N'Truong',200.0,'2003-10-10','2004-12-10',N'UDCN','007'),
+('006',N'Nghien cuu te bao goc',N'Nha nuoc',4000.0,'2006-10-12','2009-10-12',N'NCPT','004'),
+('007',N'HTTT quan ly thu vien o cac truong ƒêH',N'Truong',20.0,'2009-05-10','2010-05-10',N'QLGD','001')
 
---Nh?p data cho b?ng CONGVIEC
+--Nhap data cho bang CONGVIEC
 insert into CONGVIEC(MADT,SOTT,TENCV,NGAYBD,NGAYKT)
 values
- ('001',1,N'Kh?i t?o v‡ L?p k? ho?ch','2007-10-20','2008-12-20'),
- ('001',2,N'X·c ??nh yÍu c?u','2008-12-21','2008-03-21'),
- ('001',3,N'Ph‚n tÌch h? th?ng','2008-03-22','2008-05-22'),
- ('001',4,N'Thi?t k? h? th?ng','2008-05-23','2008-06-23'),
- ('001',5,N'C‡i ??t th? nghi?m','2008-06-24','2008-10-20'),
- ('002',1,N'Kh?i t?o v‡ l?p k? ho?ch','2009-05-10','2009-07-10'),
- ('002',2,N'X·c ??nh yÍu c?u','2009-07-11','2009-10-11'),
- ('002',3,N'Ph‚n tÌch h? th?ng','2009-10-12','2009-12-20'),
- ('002',4,N'Thi?t k? h? th?ng','2009-12-21','2010-03-22'),
- ('002',5,N'C‡i ??t th? nghi?m','2010-03-23','2010-05-10'),
- ('006',1,N'L?y m?u','2006-10-20','2007-02-20'),
- ('006',2,N'NuÙi c?y','2007-02-21','2008-09-21')
+ ('001',1,N'Khoi tao va Lap ke hoach','2007-10-20','2008-12-20'),
+ ('001',2,N'Xac dinh yeu cau','2008-12-21','2008-03-21'),
+ ('001',3,N'Phan tich he thong','2008-03-22','2008-05-22'),
+ ('001',4,N'Thiet ke he thong','2008-05-23','2008-06-23'),
+ ('001',5,N'Cai dat thu nghiem','2008-06-24','2008-10-20'),
+ ('002',1,N'Khoi tao va lap ke hoach','2009-05-10','2009-07-10'),
+ ('002',2,N'Xac dinh yeu cau','2009-07-11','2009-10-11'),
+ ('002',3,N'Phan tich he thong','2009-10-12','2009-12-20'),
+ ('002',4,N'Thiet ke he thong','2009-12-21','2010-03-22'),
+ ('002',5,N'Cai dat thu nghiem','2010-03-23','2010-05-10'),
+ ('006',1,N'Lay m·∫´u','2006-10-20','2007-02-20'),
+ ('006',2,N'Nuoi cay','2007-02-21','2008-09-21')
 
- --Nh?p data cho b?ng THAMGIADT
+ --Nhap data cho bang THAMGIADT
  Insert Into THAMGIADT(MAGV,MADT,STT,PHUCAP) 
  values
  ('001','002',1,0.0),
@@ -170,10 +170,10 @@ values
  
  Insert Into THAMGIADT(MAGV,MADT,STT,PHUCAP,KETQUA)
  values
- ('002','001',4,2.0,N'??t'),
- ('003','001',1,1.0,N'??t'),
- ('003','001',2,0.0,N'??t'),
- ('003','001',4,1.0,N'??t')
+ ('002','001',4,2.0,N'Dat'),
+ ('003','001',1,1.0,N'Dat'),
+ ('003','001',2,0.0,N'Dat'),
+ ('003','001',4,1.0,N'Dat')
 
 
  Insert Into THAMGIADT(MAGV,MADT,STT,PHUCAP) 
@@ -182,9 +182,9 @@ values
  
  Insert Into THAMGIADT(MAGV,MADT,STT,PHUCAP,KETQUA)
  values
- ('004','006',1,0.0,N'??t'),
- ('004','006',2,1.0,N'??t'),
- ('006','006',2,1.5,N'??t') 
+ ('004','006',1,0.0,N'Dat'),
+ ('004','006',2,1.0,N'Dat'),
+ ('006','006',2,1.5,N'Dat') 
 
  Insert Into THAMGIADT(MAGV,MADT,STT,PHUCAP) 
  values
@@ -192,59 +192,24 @@ values
  ('009','002',4,1.5) 
  select * from THAMGIADT
 
- --Nh?p data cho b?ng KHOA
+ --Nhap data cho bang KHOA
  Insert Into KHOA(MAKHOA,TENKHOA,NAMTL,PHONG,DIENTHOAI,TRUONGKHOA,NGAYNHANCHUC)
  values
- (N'CNTT',N'CÙng ngh? thÙng tin',1995,'B11','0838123456','002','2005-02-20'),
- (N'HH',N'HÛa h?c',1980,'B41','0838456456','007','2001-10-15'),
- (N'SH',N'Sinh h?c',1980,'B31','0838454545','004','2000-10-11'),
- (N'VL',N'V?t l˝',1976,'B21','0838223223','005','2003-09-18')
+ (N'CNTT',N'Cong nghe thong tin',1995,'B11','0838123456','002','2005-02-20'),
+ (N'HH',N'Hoa hoc',1980,'B41','0838456456','007','2001-10-15'),
+ (N'SH',N'Sinh hoc',1980,'B31','0838454545','004','2000-10-11'),
+ (N'VL',N'Vat ly',1976,'B21','0838223223','005','2003-09-18')
 
 
- select * from CHUDE
- select * from DETAI
+
  
-
- select * from DETAI
-
- --TÌnh t?ng ph? c?p c?a t?ng gi·o viÍn v‡ t?ng kinh phÌ c?a ?? t‡i m‡ gi·o viÍn ?Û ph? tr·ch
- -- B?ng c?n l?y: thamgiadt, detai
-  select * from THAMGIADT
-  select * from DETAI
-
-select THAMGIADT.MAGV, THAMGIADT.MADT, sum(THAMGIADT.PHUCAP)as TONGPC, sum(DETAI.KINHPHI) AS TONGKP 
-from THAMGIADT inner join DETAI on THAMGIADT.MADT = DETAI.MADT
-group by THAMGIADT.MAGV, THAMGIADT.MADT
-
---1/T? b?ng GIAOVIEN l?y thÙng tin B? mÙn (m„ b? mÙn, tÍn b? mÙn) c?a gi·o viÍn ph? tr·ch
- select GIAOVIEN.MAGV,GIAOVIEN.HOTEN,BOMON.MABM, BOMON.TENBM from GIAOVIEN inner join BOMON on GIAOVIEN.MABM = BOMON.MABM
-
- --2/ ?ng v?i m?i ?? t‡i , l?y t?t c? thÙng tin ch? ?? liÍn quan
- select DETAI.MADT, DETAI.TENDT, CHUDE.* from DETAI inner join CHUDE on DETAI.MACD = CHUDE.MACD
-
- --3/ ?ng v?i m?i ?? t‡i, cho bi?t thÙng tin gi·o viÍn tham gia (h? tÍn,  ??a ch?), k?t qu? ??t ???c 
- select * from DETAI
- select * from GIAOVIEN
- select DETAI.MADT, DETAI.TENDT, GIAOVIEN.HOTEN, GIAOVIEN.DIACHI from DETAI inner join GIAOVIEN on DETAI.GVCNDT = GIAOVIEN.MAGV
-
- --4/ Li?t kÍ t?t c? c·c khoa hi?n cÛ, thÙng tin tr??ng khoa ( g?i ˝: liÍn k?t v?i b?ng GIAOVIEN)
- select * from KHOA
- select KHOA.MAKHOA,KHOA.TENKHOA, KHOA.DIENTHOAI, GIAOVIEN.HOTEN, GIAOVIEN.NGSINH from KHOA left join GIAOVIEN on KHOA.TRUONGKHOA = GIAOVIEN.MAGV
-
- --5/ Li?t kÍ t?t c? c·c b? mÙn cÛ, thÙng tin tr??ng b? mÙn ( g?i ˝: liÍn k?t v?i b?ng GIAOVIEN)
- select * from BOMON
- select BOMON.MABM, BOMON.TENBM,  GIAOVIEN.HOTEN as TRUONGBOMON, GIAOVIEN.NGSINH AS NGSINH_TBM from BOMON left join GIAOVIEN on BOMON.TRUONGBM = GIAOVIEN.MAGV
-
-
-
-
- ---B‡i t?p tu?n 6 (09/12/2023)
- --1/Li?t kÍ t?t c? cÙng vi?c theo t?ng ?? t‡i, cho bi?t ng‡y b?t ??u v‡ ng‡y k?t th˙c
+ 
+ --1/Liet ke tat ca cong viec theo tung de tai, cho biet ngay bat dau va ngay ket thuc
  select * from DETAI
  select * from CONGVIEC
  select DETAI.MADT, CONGVIEC.TENCV, DETAI.TENDT, CONGVIEC.NGAYBD, CONGVIEC.NGAYKT from CONGVIEC inner join DETAI on CONGVIEC.MADT = DETAI.MADT
 
- --2/Li?t kÍ cÙng vi?c cÛ ng‡y k?t th˙c mu?n nh?t (l?n nh?t) ?ng v?i t?ng ?? t‡i.
+ --2/Liet ke cong viec co ngay ket thuc muon nhat (lon nhat) ung voi tung de tai.
 
  select max(CONGVIEC.TENCV) as TENCV,DETAI.TENDT, max(CONGVIEC.NGAYKT) as NGAYKT_MUONNHAT  from CONGVIEC inner join DETAI on CONGVIEC.MADT = DETAI.MADT 
  group by DETAI.TENDT
@@ -254,69 +219,69 @@ group by THAMGIADT.MAGV, THAMGIADT.MADT
  
 
 
---3/??m s? l??ng ?? t‡i "??t" theo t? gi·o viÍn
+--3/Dem so luong de tai "Dat" theo tung giao vien
 select * from THAMGIADT
 select * from GIAOVIEN
 
-select GIAOVIEN.MAGV,  GIAOVIEN.HOTEN, count(THAMGIADT.KETQUA)as SL_DAT from THAMGIADT inner join GIAOVIEN on THAMGIADT.MAGV = GIAOVIEN.MAGV where THAMGIADT.KETQUA = N'??t'
+select GIAOVIEN.MAGV,  GIAOVIEN.HOTEN, count(THAMGIADT.KETQUA)as SL_DAT from THAMGIADT inner join GIAOVIEN on THAMGIADT.MAGV = GIAOVIEN.MAGV where THAMGIADT.KETQUA = N'ƒê·∫°t'
 group by GIAOVIEN.MAGV,  GIAOVIEN.HOTEN
 
---4/Li?t kÍ ch? ?? theo t?ng ?? t‡i
+--4/Liet ke chu de theo tung de tai
 
 select DETAI.MADT, DETAI.TENDT, CHUDE.* from DETAI inner join CHUDE on DETAI.MACD = CHUDE.MACD
 
---5/TÌnh t?ng kinh phÌ ?? t‡i m‡ gi·o viÍn ch? nhi?m ?? t‡i (gvcndt) ???c c?p
+--5/Tinh tong kinh phi de tai ma giao vi√™n chu nhiem de tai (gvcndt) duoc cap
 select * from DETAI
 
 select  GIAOVIEN.HOTEN, sum(DETAI.KINHPHI)  from DETAI inner join GIAOVIEN on DETAI.GVCNDT = GIAOVIEN.MAGV
 group by GIAOVIEN.HOTEN
 
---6/Cho bi?t tÍn ng??i ch? nhi?m ?? t‡i c?a nh?ng ?? t‡i thu?c ch? ?? Qu?n l˝ gi·o d?c
+--6/Cho biet ten nguoi chu nhiem de tai cua nhung de tai thuoc chu de Quan ly giao duc
 
 select DETAI.MADT, DETAI.TENDT, DETAI.MACD, GIAOVIEN.HOTEN from DETAI  inner join GIAOVIEN on DETAI.GVCNDT = GIAOVIEN.MAGV where DETAI.MACD = 'QLGD'
 
---7/TÏm nh?ng gi·o viÍn l‡ tr??ng b? mÙn
+--7/Tim nhung giao vien la truong bo mon
 
 select GIAOVIEN.MAGV, GIAOVIEN.HOTEN, GIAOVIEN.PHAI, GIAOVIEN.NGSINH, BOMON.TENBM as TRUONGBOMON from BOMON inner join GIAOVIEN on BOMON.TRUONGBM = GIAOVIEN.MAGV
 
---8/TÏm gi·o viÍn cÛ l??ng cao nh?t
+--8/Tim giao vien co luong cao nhat
 
 select * from GIAOVIEN where LUONG = (select max(LUONG) from GIAOVIEN)
 
---9/ Cho bi?t tÍn khoa cÛ ?Ùng gi·o viÍn nh?t
+--9/ Cho biet ten khoa co dong giao vien nhat
 
 select KHOA.TENKHOA, count(KHOA.TENKHOA) as SOLUONG_GV into #tmp1 from BOMON inner join KHOA on BOMON.MAKHOA = KHOA.MAKHOA inner join GIAOVIEN on BOMON.MABM = GIAOVIEN.MABM
-group by KHOA.TENKHOA --HI?N RA B?NG S? L??NG GI¡O VI N CHO M?I KHOA R?I L?U V¿O B?NG T?M.
+group by KHOA.TENKHOA --HIEN RA BANG SO LUONG GIAO VIEN CHO MOI KHOA ROI LUU VAO BANG TAM.
 
 select * from #tmp1 where SOLUONG_GV = (select max(SOLUONG_GV) from #tmp1)
 
 
---10/ Cho bi?t h? tÍn gi·o viÍn ch? nhi?m nhi?u ?? t‡i nh?t
+--10/ Cho biet ho ten giao vien chu nhiem nhieu de tai nhat
 select  GIAOVIEN.HOTEN, COUNT(DETAI.TENDT)  as SOLUONG_DT into #tmp2 from DETAI inner join GIAOVIEN ON DETAI.GVCNDT = GIAOVIEN.MAGV 
-group by GIAOVIEN.HOTEN -- HI?N RA B?NG S? L??NG ?? T¿I CHO M?I GI¡O VI N R?I L?U V¿O B?NG T?M
+group by GIAOVIEN.HOTEN -- HIEN RA BANG SO LUONG DE TAI CHO MOI GIAO VIEN ROI LUU VAO BANG TAM
 
 select * from #tmp2 where SOLUONG_DT = (select max(SOLUONG_DT) from #tmp2)
 
---11/ Cho bi?t m„ b? mÙn cÛ nhi?u gi·o viÍn nh?t
+--11/ Cho biet ma bo mon co nhieu giao vien nhat
 select BOMON.MABM, BOMON.TENBM, COUNT(GIAOVIEN.GVQLCM) as GVQLCM , COUNT(BOMON.TRUONGBM) as TRUONGBM , count(GIAOVIEN.HOTEN) as HOTENGV into #tmp3 from GIAOVIEN inner join BOMON on GIAOVIEN.MABM = BOMON.MABM
-group by BOMON.MABM, BOMON.TENBM -- HI?N RA B?NG S? L??NG GVQLCM, TRUONGBM, HOTENGV CHO M?I B? M‘N R?I L?U V¿O B?NG T?M 
+group by BOMON.MABM, BOMON.TENBM -- HIEN RA BANG SO LUONG GVQLCM, TRUONGBM, HOTENGV CHO MOI BO MON ROI LUU VAO BANG TAM 
 
 alter table #tmp3
-add  SOLUONG_GV int --- T?O TH M C?T M?I CHO B?NG T?M
+add  SOLUONG_GV int --- TAO THEM COT MOI CHO BANG TAM
 
 
-update #tmp3 set  SOLUONG_GV = GVQLCM + TRUONGBM + HOTENGV --- ?i?n gi· tr? cho c?t s? l??ng gv cho m?i b? mÙn b?ng c·ch c?ng 3 GVQLCM, TRUONGBM V¿ HOTEN GV
+update #tmp3 set  SOLUONG_GV = GVQLCM + TRUONGBM + HOTENGV --- Dien gia tri cho cot so luong gv cho moi bo mon bang cach cong 3 GVQLCM, TRUONGBM V√Ä HOTEN GV
 
 
 
 select * from #tmp3 where SOLUONG_GV=(select max(SOLUONG_GV) from #tmp3)
 
---12/Cho bi?t tÍn nh?ng gi·o viÍn khoa CÙng ngh? thÙng tin m‡ ch?a tham gia ?? t‡i n‡o.
+--12/Cho biet ten nhung giao vien khoa Cong nghe thong tin ma chua tham gia de tai nao.
 
 select GIAOVIEN.MAGV, GIAOVIEN.HOTEN, KHOA.TENKHOA into #tmp4 
 from BOMON inner join KHOA on BOMON.MAKHOA = KHOA.MAKHOA inner join GIAOVIEN on BOMON.MABM = GIAOVIEN.MABM where KHOA.MAKHOA = 'CNTT'
--- L?y b?ng m„ gi·o viÍn v‡ tÍn c?a gi·o viÍn thu?c khoa CÙng ngh? thÙng tin r?i l?u v‡o b?ng t?m
+-- Lay bang ma giao vien va ten cua giao vien thuoc khoa Cong nghe thong tin roi luu vao bang tam
 
 
 select #tmp4.MAGV, #tmp4.HOTEN, #tmp4.TENKHOA from THAMGIADT right join #tmp4 on THAMGIADT.MAGV = #tmp4.MAGV where THAMGIADT.MAGV is null
---L?y b?ng m„ gi·o viÍn v‡ tÍn c?a gi·o viÍn thu?c khoa CÙng ngh? thÙng tin m‡ ch?a tham gia ?? t‡i n‡o. 
+--Lay bang ma giao vien va ten cua giao vien thuoc khoa Cong nghe thong tin ma chua tham gia de tai nao. 
